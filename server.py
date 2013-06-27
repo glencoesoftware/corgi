83d2c3883b4f49b5174f46543459b1bd58b2ac6c
Andreas Knab <knabar@gmail.com>
2013-06-27 16:23:38 +0100

    Add event listener

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 import tornado.ioloop
 import tornado.web
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+
+class EventHandler(tornado.web.RequestHandler):
+    def post(self):
+        print self.request.body
+
+
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/event", EventHandler),
 ])
 
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(19090)
     tornado.ioloop.IOLoop.instance().start()
