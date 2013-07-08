85465bf864c15eba97dab1920c1cdada4bc48805
Andreas Knab <knabar@gmail.com>
2013-07-08 10:53:24 +0200

    Better subscribe and unsubscribe scripts

--- sample-unsubscribe.sh
+++ sample-unsubscribe.sh
@@ -4,6 +4,6 @@ CALLBACK=http://envy.glencoesoftware.com:19090/event
 
 curl -u $USER -i \
   https://api.github.com/hub \
-  -F "hub.mode=subscribe" \
+  -F "hub.mode=unsubscribe" \
   -F "hub.topic=https://github.com/$REPO/events/pull_request.json" \
   -F "hub.callback=$CALLBACK"
